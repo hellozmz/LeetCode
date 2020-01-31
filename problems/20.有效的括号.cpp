a389f4f798ea9952a8b3838b1cb29c8ec61400a4
zhumingzhu1 <zhumingzhu@jd.com>
2020-02-01 00:01:36 +0800

    更新括号匹配的算法，使用map简化code

--- problems/20.有效的括号.cpp
+++ problems/20.有效的括号.cpp
@@ -7,48 +7,27 @@
 // @lc code=start
 class Solution {
 public:
+    std::map<char, char> char_map{
+        {')', '('},
+        {']', '['},
+        {'}', '{'},
+    };
     bool isValid(string s) {
         if (s.size() == 0)  return true;
         std::stack<char> st;
         for (int i=0; i<s.size(); ++i) {
-            // std::cout << "s[" << i << "]=" << s[i] << endl;
-            if (s[i] == '(' ||
-                s[i] == '{' ||
-                s[i] == '[') {
-                st.push(s[i]);
-                // std::cout << st.size() << std::endl;
-            } else if (s[i] == ')') {
-                if (!st.empty() && st.top() == '(') {
-                    // std::cout << st.top() << endl;
+            char cur_char = s[i];
+            if (char_map.find(cur_char) == char_map.end()) {
+                st.push(cur_char);
+            } else {
+                if (!st.empty() && (st.top() == char_map.at(cur_char))) {
                     st.pop();
                 } else {
-                    // std::cout << "()" << std::endl;
-                    return false;
-                }
-            } else if (s[i] == '}') {
-                if (!st.empty() && st.top() == '{') {
-                    // std::cout << st.top() << endl;
-                    st.pop();
-                } else {
-                    // std::cout << "{}" << std::endl;
-                    return false;
-                }
-            } else if (s[i] == ']') {
-                if (!st.empty() && st.top() == '[') {
-                    // std::cout << st.top() << endl;
-                    st.pop();
-                } else {
-                    // std::cout << "[]" << std::endl;
                     return false;
                 }
             }
         }
 
-        // std::cout << "st.size()=" << st.size() << std::endl;
-        // while (st.size() != 0) {
-        //     std::cout << st.top() << std::endl;
-        //     st.pop();
-        // }
         return st.size()==0 ? true : false;
     }
 };
