0bf25420efc4c2220a8060c49482677f0f18965c
hellozmz <407190054@qq.com>
2022-07-06 22:35:44 +0800

    dfs

---
+++ Stronger/226.翻转二叉树.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode.cn/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (79.20%)
+ * Likes:    1337
+ * Dislikes: 0
+ * Total Accepted:    487.9K
+ * Total Submissions: 615.7K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 4
+ * 2 7
+ * 1 3 6 9
+ * 输出：[4,7,2,9,6,3,1]
+ * 4
+ * 7 2
+ * 9 6 3 1
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+
+    void dfs(TreeNode* root) {
+        if (root != nullptr) {
+            auto left = root->left;
+            root->left = root->right;
+            root->right = left;
+            dfs(root->left);
+            dfs(root->right);
+        } else {
+            return;
+        }
+    }
+};
+// @lc code=end
+
