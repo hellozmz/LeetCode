026991777ae19cb0bf64a977df7c0e3576130fe4
hellozmz <407190054@qq.com>
2022-10-07 16:12:07 +0800

    构建测试环境

---
+++ Stronger/header.hpp
@@ -0,0 +1,5 @@
+
+#include <algorithm>
+#include <climits>
+#include <vector>
+using namespace std;
---
+++ Stronger/test.cc
@@ -0,0 +1,12 @@
+#include "./45.跳跃游戏-ii.cpp"
+
+#include <algorithm>
+#include <climits>
+#include <vector>
+using namespace std;
+
+int main() {
+    Solution s;
+    vector<int> input{2,3,1,1,4};
+    s.jump(input);
+}
