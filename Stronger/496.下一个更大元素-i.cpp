d079bb61edb57285c30ab377c59fcb7d1ca0af5b
hellozmz <407190054@qq.com>
2022-10-26 23:05:00 +0800

    单调栈

---
+++ Stronger/496.下一个更大元素-i.cpp
@@ -0,0 +1,130 @@
+/*
+ * @lc app=leetcode.cn id=496 lang=cpp
+ *
+ * [496] 下一个更大元素 I
+ *
+ * https://leetcode.cn/problems/next-greater-element-i/description/
+ *
+ * algorithms
+ * Easy (71.68%)
+ * Likes:    862
+ * Dislikes: 0
+ * Total Accepted:    224K
+ * Total Submissions: 312.4K
+ * Testcase Example:  '[4,1,2]\n[1,3,4,2]'
+ *
+ * nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+ * 
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+ * 
+ * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定
+ * nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+ * 
+ * 返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出：[-1,3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * - 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+ * - 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出：[3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+ * - 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 10^4
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // 记录nums2下标索引id
+        stack<int> st;
+        int len = nums1.size();
+        int len2 = nums2.size();
+        vector<int> result(len, -1);
+        unordered_map<int, int> mapping;
+
+        for (int i = 0; i < nums1.size(); ++i) {
+            mapping[nums1[i]] = i;
+        }
+
+        st.push(0);
+        for (int i = 1; i < len2; ++i) {
+            if (nums2[i] <= nums2[st.top()]) {
+                st.push(i);
+            } else {
+                // nums2 中值大于原来的，需要更新
+                while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                    if (mapping.count(nums2[st.top()]) > 0) {
+                        result[mapping[nums2[st.top()]]] = nums2[i];
+                    }
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> result(nums1.size(), -1);
+        if (nums1.size() == 0) return result;
+
+        unordered_map<int, int> umap; // key:下标元素，value：下标
+        for (int i = 0; i < nums1.size(); i++) {
+            umap[nums1[i]] = i;
+        }
+        st.push(0);
+        for (int i = 1; i < nums2.size(); i++) {
+            if (nums2[i] < nums2[st.top()]) {           // 情况一
+                st.push(i);
+            } else if (nums2[i] == nums2[st.top()]) {   // 情况二
+                st.push(i);
+            } else {                                    // 情况三
+                while (!st.empty() && nums2[i] > nums2[st.top()]) {
+                    if (umap.count(nums2[st.top()]) > 0) { // 看map里是否存在这个元素
+                        int index = umap[nums2[st.top()]]; // 根据map找到nums2[st.top()] 在 nums1中的下标
+                        result[index] = nums2[i];
+                    }
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return result;
+    }
+};
---
+++ Stronger/516.最长回文子序列.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=cpp
+ *
+ * [516] 最长回文子序列
+ *
+ * https://leetcode.cn/problems/longest-palindromic-subsequence/description/
+ *
+ * algorithms
+ * Medium (66.93%)
+ * Likes:    894
+ * Dislikes: 0
+ * Total Accepted:    152.1K
+ * Total Submissions: 227.2K
+ * Testcase Example:  '"bbbab"'
+ *
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * 
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "bbbab"
+ * 输出：4
+ * 解释：一个可能的最长回文子序列为 "bbbb" 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "cbbd"
+ * 输出：2
+ * 解释：一个可能的最长回文子序列为 "bb" 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * s 仅由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int len = s.size();
+        vector<vector<int>> dp(len, vector<int>(len, 0));
+        for (int i = 0; i < len; ++i) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = len - 1; i >= 0; --i) {
+            for (int j = i + 1; j < len; ++j) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[0][len - 1];
+    }
+};
+// @lc code=end
